36c8c3e9da44c7bed69b2b1bf762770fe6821463
transparent-stories <transparentstoriess@gmail.com>
2024-12-03 21:55:45 +0530

    AOS issue resolve

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { AOSInit } from '@/utils/aos';
 import '../app/globals.css';
 import Providers from './providers'; // Import the new client component
 
@@ -5,6 +6,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <AOSInit />
       <head>
         {/* Add Google Font (Sora) */}
         <link
@@ -12,7 +14,6 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body>
         <Providers>
@@ -24,10 +25,6 @@ export default function RootLayout({ children }) {
             <p>© 2024 Genki Ramune</p>
           </footer>
         </Providers>
-        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-        <script>
-          AOS.init();
-        </script>
       </body>
     </html>
   );
