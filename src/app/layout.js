9d026ccb383361019da229d9261e99ce5a554ce1
transparent-stories <transparentstoriess@gmail.com>
2024-12-03 21:43:15 +0530

    font and aos issue resolve

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,7 @@
-'use client';
-import { useEffect } from 'react';
 import '../app/globals.css';
 import Providers from './providers'; // Import the new client component
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    AOS.init({
-      duration: 800,
-      once: false,
-    })
-  }, [])
 
   return (
     <html lang="en">
@@ -22,6 +12,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body>
         <Providers>
@@ -33,6 +24,10 @@ export default function RootLayout({ children }) {
             <p>© 2024 Genki Ramune</p>
           </footer>
         </Providers>
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script>
+          AOS.init();
+        </script>
       </body>
     </html>
   );
